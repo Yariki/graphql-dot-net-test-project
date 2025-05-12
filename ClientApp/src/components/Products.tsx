5de37938d9748d020f0d39925e793b9fc47d4a1a
Yaroslav Yariki Lisovskyi <iyariki.ya@gmail.com>
2025-05-12 20:04:08 +0300

    implemented new ProductsTable component, and populate it with products according to selected category

--- ClientApp/src/components/Products.tsx
+++ ClientApp/src/components/Products.tsx
@@ -8,27 +8,15 @@ import type { OptionOnSelectData, SelectionEvents, SelectProps } from "@fluentui
 
 import { ProductsTable } from './ProductsTable';
 
-import {
-    TableBody,
-    TableCell,
-    TableRow,
-    Table,
-    TableHeader,
-    TableHeaderCell,
-    TableCellLayout,
-    PresenceBadgeStatus,
-    Avatar,
-} from "@fluentui/react-components";
-import { on } from 'process';
-import { gql, useQuery } from '@apollo/client';
+interface CatalogValues {
+    id: string | undefined;
+    name: string | undefined;
+}
 
 export const  Products : React.FC = () => {
     const { data, loading, error } = useGetCatalogsQuery();
-    const [value, setValue] = React.useState<string | undefined>(undefined);
-    const [idValue, setIdValue] = React.useState<string | undefined>(undefined);
-    const [products, setProducts] = React.useState<any[]>([]);
-    const [loadingProducts, setLoadingProducts] = React.useState<boolean>(false);
-    const [errorProducts, setErrorProducts] = React.useState<string | undefined>(undefined);
+    const [value, setValue] = React.useState<CatalogValues>({ id: "", name: "" });
+
     
 
     if (loading) {
@@ -40,31 +28,14 @@ export const  Products : React.FC = () => {
     }
 
     const onOptionSelect = (event: SelectionEvents, optionData: OptionOnSelectData) => {
-        setValue(optionData.optionText);
-        setIdValue(optionData.optionValue);
-
-        const fetchProducts = async () => {
-            const { data, loading, error } = useQuery(
-                GET_PRODUCTS,
-                {
-                    variables: { catalogId: optionData.optionValue },
-                    fetchPolicy: 'network-only',
-                }
-            );
-            setLoadingProducts(loading);
-            setErrorProducts(error?.message);
-            setProducts(data?.products?.nodes || []);
-        }
-
-        fetchProducts();
-
+        setValue({id: optionData.optionValue, name: optionData.optionText});
     };
 
     return (
         <React.Fragment>
             <div>
                 <Label htmlFor='catalogs-select'>Catalogs</Label>
-                <Combobox id='catalogs-select' onOptionSelect={onOptionSelect} value={value} defaultValue={undefined} placeholder="Select a catalog">
+                <Combobox id='catalogs-select' onOptionSelect={onOptionSelect} value={value?.name} defaultValue={""} placeholder="Select a catalog">
                     {
                         data.catalogs?.nodes?.map((catalog) => (
                             <Option key={catalog?.id} value={catalog?.id}>
@@ -74,7 +45,7 @@ export const  Products : React.FC = () => {
                     }
                 </Combobox>
             </div>
-            <ProductsTable catalogId={idValue} />
+            <ProductsTable catalogId={value?.id} />
         </React.Fragment>
     );
 
--- ClientApp/src/components/ProductsTable.tsx
+++ ClientApp/src/components/ProductsTable.tsx
@@ -1,32 +1,34 @@
 import React from "react";
-import { Table, TableHeader, TableHeaderCell, TableRow, TableCellLayout, PresenceBadgeStatus, Avatar } from "@fluentui/react-components";
+import { Table, TableHeader, TableHeaderCell, TableRow, TableCellLayout, PresenceBadgeStatus, Avatar, TableBody, TableCell } from "@fluentui/react-components";
 import {gql, useQuery} from "@apollo/client";
 
-const GET_PRODUCTS = gql(`query getProducts($catalogId: ID!) {
-    products(catalogId: $catalogId) {
+const GET_PRODUCTS = gql(`query getProducts($catalogId: Int) {
+    products(where : {catalogId: {eq: $catalogId}}) {
         nodes {
             id
             name
             price
-            descrioption
+            description
             catalogId
         }
     }
 }`);
 
 export interface ProductsTableProps {
-    catalogId: string | undefined;
+    catalogId: string;
 }
 
 
 export const ProductsTable: React.FC<ProductsTableProps> = ({ catalogId }) => {
-    
+
+    catalogId = !catalogId || catalogId !== "" ? catalogId : "-1";
+
     const { data, loading, error } = useQuery(GET_PRODUCTS, {
         variables: { catalogId },
         fetchPolicy: 'network-only',
     });
 
-
+    console.log("Error: " + error);
 
     return (
         <>
@@ -35,12 +37,28 @@ export const ProductsTable: React.FC<ProductsTableProps> = ({ catalogId }) => {
                     <TableRow>
                         <TableHeaderCell>Product</TableHeaderCell>
                         <TableHeaderCell>Price</TableHeaderCell>
-                        <TableHeaderCell>desclription</TableHeaderCell>
+                        <TableHeaderCell>Description</TableHeaderCell>
                     </TableRow>
                 </TableHeader>
                 {loading && <p>Loading...</p>}
                 {error && <p>Error</p>}
+                {!catalogId && <p>Please select a catalog</p>}
                 
+                <TableBody>
+                    {data?.products?.nodes?.map((product: any) => (
+                        <TableRow key={product.id}>
+                            <TableCell>
+                                <TableCellLayout
+                                    media={<Avatar name={product.name} shape="square" size={40} style={{ width: '100px' }} />}
+                                    content={product.name}
+                                    description={product.description}
+                                />
+                            </TableCell>
+                            <TableCell>{product.price}</TableCell>
+                            <TableCell>{product.description}</TableCell>
+                        </TableRow>
+                    ))}
+                </TableBody>
 
             </Table>
         </>
